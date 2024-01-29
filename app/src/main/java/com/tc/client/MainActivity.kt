e96aa19f79bcf2ad04e00b678c42888c289ce554
hy <hy@gmail.com>
2024-01-29 12:00:56 +0800

    add a simple button
    Signed-off-by: hy <hy@gmail.com>

--- app/src/main/java/com/tc/client/MainActivity.kt
+++ app/src/main/java/com/tc/client/MainActivity.kt
@@ -5,16 +5,42 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.tc.client.theme.ThunderCloudTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContent {
+            ThunderCloudTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Button(
+
+                        onClick = {
+                            startFrameActivity()
+                        }
+                    ) {
+
+                    }
+                }
+            }
+        }
+    }
+
+    fun startFrameActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, FrameRenderActivity::class.java))
         }, 300);
-
     }
 
 }
