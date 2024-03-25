0d0af2ff3c4053b0efef6f6141ce90bdfab14461
hy <hy@gmail.com>
2024-03-25 17:17:57 +0800

    add admob but not work
    Signed-off-by: hy <hy@gmail.com>

---
+++ app/src/main/java/com/tc/client/App.java
@@ -0,0 +1,31 @@
+package com.tc.client;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.AdapterStatus;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.Map;
+
+public class App extends Application {
+    private static final String TAG = "Main";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                Log.i(TAG, "onInitializationComplete");
+                Map<String, AdapterStatus> status = initializationStatus.getAdapterStatusMap();
+                for (String key: status.keySet()) {
+                    Log.i(TAG, "==> Ad init status: " + key + ", " + status.get(key).getInitializationState().name());
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/tc/client/MainActivity.java
@@ -0,0 +1,30 @@
+package com.tc.client;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.AdapterStatus;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.id_start_stream).setOnClickListener(v -> {
+            startActivity(new Intent(this, FrameRenderActivity.class));
+        });
+    }
+}
